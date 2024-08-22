01ac7fb91a0c004ad9df1a8fa626a0569aa13cca
beltranjohnpaulo <beltranjohnpaulo@gmail.com>
2024-08-22 22:37:49 +0800

    added getProfile

--- controllers/user.js
+++ controllers/user.js
@@ -65,3 +65,23 @@ module.exports.loginUser = (req, res) => {
 		return res.status(400).send({ message: 'Invalid Email' })
 	}
 }
+
+
+module.exports.getProfile = (req, res) => {
+	console.log(req.user)
+	return User.findById(req.user.id)
+	.then(user => {
+	    if (!user) {
+	        return res.status(404).send({ error: 'User not found' });
+	    }
+
+	    user.password = undefined;
+
+	    return res.status(200).send({ user });
+	})
+	.catch(err => {
+		console.error("Error in fetching user profile", err)
+		return res.status(500).send({ error: 'Failed to fetch user profile' })
+	});
+
+};
